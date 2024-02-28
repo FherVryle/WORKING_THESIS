eece6810126185c8980590a927cf32fa7cb74a5c
ShoSho <110207604+FherVryle@users.noreply.github.com>
2024-02-29 01:02:30 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'homepage/static/'
+STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
 
